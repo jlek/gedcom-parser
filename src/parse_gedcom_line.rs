fb7f7a37036eceecc28a82fe51d65b05530a944c
John <jlek@findmypast.com>
2020-04-27 16:45:22 +0100

    Added parse_tag

--- src/parse_gedcom_line.rs
+++ src/parse_gedcom_line.rs
@@ -1,4 +1,10 @@
-use nom::{bytes::complete::take_while_m_n, combinator::map_res, IResult};
+use nom::{
+  bytes::complete::{tag as specific_characters, take_while, take_while_m_n},
+  combinator::map_res,
+  IResult,
+};
+
+const DELIM: &str = " "; // Should be 0x20
 
 #[derive(Debug, PartialEq)]
 pub struct GedcomLine {
@@ -15,12 +21,24 @@ fn from_decimal(input: &str) -> Result<u8, std::num::ParseIntError> {
   u8::from_str_radix(input, 10)
 }
 
-fn is_decimal_digit(c: char) -> bool {
-  c.is_digit(10)
+fn is_decimal_digit(character: char) -> bool {
+  character.is_digit(10)
 }
 
 fn parse_level(input: &str) -> IResult<&str, u8> {
   map_res(take_while_m_n(1, 2, is_decimal_digit), from_decimal)(input)
+}
+
+// ===
+// Tag
+// ===
+
+fn is_alphanumeric(character: char) -> bool {
+  character.is_alphanumeric()
+}
+
+fn parse_tag(input: &str) -> IResult<&str, &str> {
+  take_while_m_n(1, 32, is_alphanumeric)(input)
 }
 
 // ==========
@@ -29,10 +47,19 @@ fn parse_level(input: &str) -> IResult<&str, u8> {
 
 fn parse_gedcom_line(input: &str) -> IResult<&str, GedcomLine> {
   let (input, level) = parse_level(input)?;
-  let tag = "".to_string();
+  let (input, _) = specific_characters(DELIM)(input)?;
+  let (input, tag) = parse_tag(input)?;
+  let (input, _) = specific_characters(DELIM)(input)?;
   let value = "".to_string();
 
-  Ok((input, GedcomLine { level, tag, value }))
+  Ok((
+    input,
+    GedcomLine {
+      level,
+      tag: tag.to_string(),
+      value,
+    },
+  ))
 }
 
 #[test]
@@ -87,8 +114,34 @@ fn parse_level_invalid_input() {
 }
 
 #[test]
+fn parse_tag_valid() {
+  // Arrange
+  let input = "TAGG Some value";
+
+  // Act
+  let (remaining_text, tag) = parse_tag(input).unwrap();
+
+  // Assert
+  assert_eq!(remaining_text, " Some value");
+  assert_eq!(tag, "TAGG");
+}
+
+#[test]
+fn parse_tag_too_long() {
+  // Arrange
+  let input = "TAGTAGTAGTAGTAGTAGTAGTAGTAGTAGTAG Some value"; // Tag is more than 32 characters
+
+  // Act
+  let (remaining_text, tag) = parse_tag(input).unwrap();
+
+  // Assert
+  assert_eq!(remaining_text, "G Some value");
+  assert_eq!(tag, "TAGTAGTAGTAGTAGTAGTAGTAGTAGTAGTA");
+}
+
+#[test]
 #[ignore]
-fn parse_color() {
+fn parse_gedcom_line_valid() {
   // Arrange
   let input = "0 TAGG Some value";
 
