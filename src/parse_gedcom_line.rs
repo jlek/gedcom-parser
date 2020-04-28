d75dd1d8878431e3fbb4fa36bd8ef00493ca0ef5
John <jlek@findmypast.com>
2020-04-28 08:55:49 +0100

    Finished first stab at parse_gedcom_line.

--- src/parse_gedcom_line.rs
+++ src/parse_gedcom_line.rs
@@ -1,5 +1,6 @@
 use nom::{
-  bytes::complete::{tag as specific_characters, take_while, take_while_m_n},
+  branch::alt,
+  bytes::complete::{tag as specific_characters, take_till, take_while_m_n},
   combinator::map_res,
   IResult,
 };
@@ -42,108 +43,61 @@ fn parse_tag(input: &str) -> IResult<&str, &str> {
 }
 
 // ==========
+// Line value
+// ==========
+
+// TODO Can we use parse_terminator for this somehow? Just so we have a single source of truth.
+fn is_terminator(character: char) -> bool {
+  character == '\n' || character == '\r'
+}
+
+fn parse_line_value(input: &str) -> IResult<&str, &str> {
+  take_till(is_terminator)(input)
+}
+
+// ==========
+// Whitespace
+// ==========
+
+fn parse_delim(input: &str) -> IResult<&str, &str> {
+  specific_characters(DELIM)(input)
+}
+
+fn parse_terminator(input: &str) -> IResult<&str, &str> {
+  alt((
+    specific_characters("\r\n"),
+    specific_characters("\n\r"),
+    specific_characters("\r"),
+    specific_characters("\n"),
+  ))(input)
+}
+
+// ==========
 // GedcomLine
 // ==========
 
 fn parse_gedcom_line(input: &str) -> IResult<&str, GedcomLine> {
   let (input, level) = parse_level(input)?;
-  let (input, _) = specific_characters(DELIM)(input)?;
+  let (input, _) = parse_delim(input)?;
   let (input, tag) = parse_tag(input)?;
-  let (input, _) = specific_characters(DELIM)(input)?;
-  let value = "".to_string();
+  let (input, _) = parse_delim(input)?;
+  let (input, value) = parse_line_value(input)?;
+  let (input, _) = parse_terminator(input)?;
 
   Ok((
     input,
     GedcomLine {
       level,
       tag: tag.to_string(),
-      value,
+      value: value.to_string(),
     },
   ))
 }
 
 #[test]
-fn parse_level_single_digit() {
-  // Arrange
-  let input = "0 TAGG Some value";
-
-  // Act
-  let (remaining_text, level) = parse_level(input).unwrap();
-
-  // Assert
-  assert_eq!(remaining_text, " TAGG Some value");
-  assert_eq!(level, 0);
-}
-
-#[test]
-fn parse_level_two_digits() {
-  // Arrange
-  let input = "42 TAGG Some value";
-
-  // Act
-  let (remaining_text, level) = parse_level(input).unwrap();
-
-  // Assert
-  assert_eq!(remaining_text, " TAGG Some value");
-  assert_eq!(level, 42);
-}
-
-#[test]
-fn parse_level_three_digits() {
-  // Arrange
-  let input = "123 TAGG Some value";
-
-  // Act
-  let (remaining_text, level) = parse_level(input).unwrap();
-
-  // Assert
-  assert_eq!(remaining_text, "3 TAGG Some value");
-  assert_eq!(level, 12);
-}
-
-#[test]
-fn parse_level_invalid_input() {
-  // Arrange
-  let input = "invalid input";
-
-  // Act
-  let result = parse_level(input);
-
-  // Assert
-  assert!(result.is_err());
-}
-
-#[test]
-fn parse_tag_valid() {
-  // Arrange
-  let input = "TAGG Some value";
-
-  // Act
-  let (remaining_text, tag) = parse_tag(input).unwrap();
-
-  // Assert
-  assert_eq!(remaining_text, " Some value");
-  assert_eq!(tag, "TAGG");
-}
-
-#[test]
-fn parse_tag_too_long() {
-  // Arrange
-  let input = "TAGTAGTAGTAGTAGTAGTAGTAGTAGTAGTAG Some value"; // Tag is more than 32 characters
-
-  // Act
-  let (remaining_text, tag) = parse_tag(input).unwrap();
-
-  // Assert
-  assert_eq!(remaining_text, "G Some value");
-  assert_eq!(tag, "TAGTAGTAGTAGTAGTAGTAGTAGTAGTAGTA");
-}
-
-#[test]
-#[ignore]
 fn parse_gedcom_line_valid() {
   // Arrange
-  let input = "0 TAGG Some value";
+  let input = "0 TAG Some value\n";
 
   // Act
   let (remaining_text, gedcom_line) = parse_gedcom_line(input).unwrap();
@@ -154,8 +108,64 @@ fn parse_gedcom_line_valid() {
     gedcom_line,
     GedcomLine {
       level: 0,
-      tag: "TAGG".to_string(),
+      tag: "TAG".to_string(),
       value: "Some value".to_string(),
     }
   );
+}
+
+#[test]
+fn parse_gedcom_line_double_digit_level() {
+  // Arrange
+  let input = "10 TAG Some value\n";
+
+  // Act
+  let (remaining_text, gedcom_line) = parse_gedcom_line(input).unwrap();
+
+  // Assert
+  assert_eq!(remaining_text, "");
+  assert_eq!(
+    gedcom_line,
+    GedcomLine {
+      level: 10,
+      tag: "TAG".to_string(),
+      value: "Some value".to_string(),
+    }
+  );
+}
+
+#[test]
+fn parse_gedcom_line_no_level() {
+  // Arrange
+  let input = "TAG Some value\n";
+
+  // Act
+  let result = parse_gedcom_line(input);
+
+  // Assert
+  assert!(result.is_err());
+}
+
+#[test]
+fn parse_gedcom_line_level_too_long() {
+  // Arrange
+  let input = "123 TAG Some value\n";
+
+  // Act
+  let result = parse_gedcom_line(input);
+
+  // Assert
+  assert!(result.is_err());
+}
+
+#[test]
+fn parse_gedcom_line_tag_too_long() {
+  // Arrange
+  let input = "0 TAGTAGTAGTAGTAGTAGTAGTAGTAGTAGTAG Some value"; // Tag is more than 32 characters
+
+  // Act
+  let result = parse_gedcom_line(input);
+
+  // Assert
+  assert!(result.is_err());
 }
