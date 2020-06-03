7eb5d8039b2ff6724baaa113518df225d15c3ab9
John <jlek@findmypast.com>
2020-06-03 16:58:53 +0100

    Added some lines to read in a test file.
    Seems to work OK.

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,12 @@ mod gedcom_entities;
 mod parsers;
 mod tests;
 
-fn main() {}
+use gedcom_data_format::from_str;
+use gedcom_entities::Record;
+use std::fs::read_to_string;
+
+fn main() {
+  let file = read_to_string("src/tests/one-node.ged").expect("File should exist");
+  let records: Vec<Record> = from_str(&file).expect("testy no crashy");
+  println!("{:?}", records);
+}
