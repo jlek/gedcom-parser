6e7325f128bafe08a8c0a93a93caa137c9219f5c
John <jlek@findmypast.com>
2020-06-08 16:52:36 +0100

    Now accepting file paths as command line paths.

--- README.md
+++ README.md
@@ -5,11 +5,10 @@ This submission is not complete: it only works for the single-node Gedcom file. 
 ## To Run
 
 1. [Install Rust](https://www.rust-lang.org/tools/install)
-2. Put the source and target file paths into `main.rs`
-3. Build: `cargo build --release`
-4. `target/release/gedcom-parser`
+2. Build: `cargo build --release`
+3. `target/release/gedcom-parser SOURCE_FILE_PATH TARGET_FILE_PATH` (_e.g._ `target/release/gedcom-parser src/tests/one-node.ged src/tests/one-node.json`)
 
-## To Run the Test
+## To Run the Tests
 
 1. [Install Rust](https://www.rust-lang.org/tools/install)
 2. `cargo test`
--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,24 @@ mod relation_entities;
 mod tests;
 mod transform_gedcom_to_relation;
 
+use clap::{App, Arg};
 use gedcom_data_format::from_str;
 use gedcom_entities::Record;
 use serde_json::to_string_pretty;
 use std::fs::{read_to_string, write};
 use transform_gedcom_to_relation::transform_gedcom_to_relation;
 
-const SOURCE_FILE_PATH: &'static str = "src/tests/one-node.ged";
-const TARGET_FILE_PATH: &'static str = "src/tests/one-node.json";
-
 fn main() {
-  let file = read_to_string(SOURCE_FILE_PATH).expect("File should exist");
+  let app = App::new("Gedcom Parser")
+    .arg(Arg::with_name("source").required(true))
+    .arg(Arg::with_name("target").required(true));
+  let matches = app.get_matches();
+  let source_file_path = matches.value_of("source").unwrap();
+  let target_file_path = matches.value_of("target").unwrap();
+
+  let file = read_to_string(source_file_path).expect("File should exist");
   let records: Vec<Record> = from_str(&file).expect("program no crashy");
   let family_tree = transform_gedcom_to_relation(&records);
   let json = to_string_pretty(&family_tree).expect("program no crashy");
-  write(TARGET_FILE_PATH, json).expect("program no crashy");
+  write(target_file_path, json).expect("program no crashy");
 }
